cda20465639ed49f7d87d4f39fb18bfd18d147bb
vvebdino <66421877+vvebdino@users.noreply.github.com>
2021-06-07 18:19:10 +0300

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function() {
+	
+	//слайдер для rs-slider
+
+if ($('.rs-slider-container').length > 0) {
+	$('.rs-slider-container').slick({
+		infinite: true,
+		slidesToShow: 1,
+		speed: 800,
+		dots: true,
+		autoplay: true,
+		lazyLoad: 'ondemand',
+		cssEase: 'cubic-bezier(0.845, 0.045, 0.355, 1)'
+	});
+}	
+
+})
